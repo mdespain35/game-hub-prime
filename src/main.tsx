96b18988242bbb6db368c04bf1809a6d9cc12e48
MICHAEL DE SPAIN <mdespain35@gmail.com>
2024-10-14 09:24:27 -0700

    Updated typo in file name

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import App from "./App.tsx";
-import theme from "./them.ts";
+import theme from "./theme.ts";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
